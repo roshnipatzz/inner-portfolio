176e984baace6226e4b05c78ddabeb07a7b6aa7a
roshnipatzz <rosh.patel@mail.utoronto.ca>
2016-08-16 17:22:52 -0400

    Front page and Functions
    - added slider code correctly, created a query (as it wasn’t displayed
    all recent posts before)
    
    -added comments to the front-page
    
    - added comments to functions.php about font enqueued and slider code

--- front-page.php
+++ front-page.php
@@ -1,8 +1,7 @@
 <?php
 /** 
- *  
- * Template Name: Front Page
- *  
+ * Template Name: Front-Page 
+ * Template displaying front page (home page) of website
  *
  */
 
@@ -13,44 +12,22 @@ get_header(); ?>
 
 <div id="primary" class="content-area">
 <main id="main" class="site-main" role="main">
-
+<!-- Following slider code taken from FlexSlider 2 Plugin on http://flexslider.woothemes.com/ to create slider --!>
 <div class="flexslider">
   <ul class="slides">
-    <li data-thumb="slide1-thumb.jpg">
+  <?php 
+  // this query displays the featured images of the most recent posts
+  $slider_query = new WP_Query(array( 'cat'=> '', 'posts_per_page' => 7) );
+		// the code checks to see if and while posts are present, they should be posted 
+		if ($slider_query->have_posts()): while ($slider_query->have_posts()): $slider_query->the_post(); ?>
+    <li>
       <a href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>">
+       <!-- this piece of code displays the image of the recent post on the slider--!>
         <?php the_post_thumbnail(); ?>
     </a>
     </li>
-    <li data-thumb="slide2-thumb.jpg">
-      <a href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>">
-        <?php the_post_thumbnail(); ?>
-    </a>
-    </li>
-    <li data-thumb="slide3-thumb.jpg">
-      <a href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>">
-        <?php the_post_thumbnail(); ?>
-    </a>
-    </li>
-    <li data-thumb="slide4-thumb.jpg">
-      <a href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>">
-        <?php the_post_thumbnail(); ?>
-    </a>
-    </li>
-     <li data-thumb="slide5-thumb.jpg">
-      <a href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>">
-        <?php the_post_thumbnail(); ?>
-    </a>
-    </li>
-    <li data-thumb="slide6-thumb.jpg">
-      <a href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>">
-        <?php the_post_thumbnail(); ?>
-    </a>
-    </li>
-    <li data-thumb="slide7-thumb.jpg">
-      <a href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>">
-        <?php the_post_thumbnail(); ?>
-    </a>
-    </li>
+    <?php endwhile; wp_reset_postdata(); //this code resets post data
+		endif; //this ends the if statement ?>
   </ul>
 </div>
 
